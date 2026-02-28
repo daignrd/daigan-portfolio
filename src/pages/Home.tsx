dfbef762541a7fab299aacbcf39fa8ac6f67025c
Daigan <67317923+daignrd@users.noreply.github.com>
2026-02-28 13:31:05 +0900

    feat: Add interactive SVG blob portrait effect to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -55,17 +55,53 @@ export default function Home() {
           </div>
 
           {/* Portrait */}
+          {/* Portrait */}
           <motion.div
             initial={{ opacity: 0, scale: 0.96 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.7, delay: 0.2, ease: [0.22, 1, 0.36, 1] }}
-            className="shrink-0 w-48 md:w-56"
+            className="shrink-0 blob-portrait-wrapper w-64 md:w-72"
           >
-            <img
-              src="/Daigan Manga Headshot BW 2026.png"
-              alt="Daigan"
-              className="w-full rounded-2xl border border-[--color-ink]/8 grayscale"
-            />
+            <svg
+              className="blob-portrait"
+              viewBox="0 0 200 200"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-labelledby="t"
+            >
+              <image
+                href="/Daigan Manga Headshot BW 2026.png"
+                width="200" height="200"
+                preserveAspectRatio="xMidYMid slice"
+                clipPath="url(#blobClip)"
+                className="grayscale"
+              />
+              <clipPath id="blobClip">
+                <path d="M43.1,-68.5C56.2,-58.6,67.5,-47.3,72.3,-33.9C77.2,-20.5,75.5,-4.9,74.2,11.3C72.9,27.6,71.9,44.5,63.8,57.2C55.7,69.8,40.6,78.2,25.5,79.2C10.4,80.1,-4.7,73.6,-20.9,69.6C-37.1,65.5,-54.5,63.9,-66,54.8C-77.5,45.8,-83.2,29.3,-85.7,12.3C-88.3,-4.8,-87.7,-22.3,-79.6,-34.8C-71.5,-47.3,-55.8,-54.9,-41.3,-64.2C-26.7,-73.6,-13.4,-84.7,0.8,-86C15,-87.2,29.9,-78.5,43.1,-68.5Z"
+                  transform="translate(100 100)" />
+              </clipPath>
+
+              <path
+                className="blob"
+                d="M43.1,-68.5C56.2,-58.6,67.5,-47.3,72.3,-33.9C77.2,-20.5,75.5,-4.9,74.2,11.3C72.9,27.6,71.9,44.5,63.8,57.2C55.7,69.8,40.6,78.2,25.5,79.2C10.4,80.1,-4.7,73.6,-20.9,69.6C-37.1,65.5,-54.5,63.9,-66,54.8C-77.5,45.8,-83.2,29.3,-85.7,12.3C-88.3,-4.8,-87.7,-22.3,-79.6,-34.8C-71.5,-47.3,-55.8,-54.9,-41.3,-64.2C-26.7,-73.6,-13.4,-84.7,0.8,-86C15,-87.2,29.9,-78.5,43.1,-68.5Z"
+                transform="translate(100 100)"
+                fill="none"
+              />
+              <path
+                id="text" d="M43.1,-68.5C56.2,-58.6,67.5,-47.3,72.3,-33.9C77.2,-20.5,75.5,-4.9,74.2,11.3C72.9,27.6,71.9,44.5,63.8,57.2C55.7,69.8,40.6,78.2,25.5,79.2C10.4,80.1,-4.7,73.6,-20.9,69.6C-37.1,65.5,-54.5,63.9,-66,54.8C-77.5,45.8,-83.2,29.3,-85.7,12.3C-88.3,-4.8,-87.7,-22.3,-79.6,-34.8C-71.5,-47.3,-55.8,-54.9,-41.3,-64.2C-26.7,-73.6,-13.4,-84.7,0.8,-86C15,-87.2,29.9,-78.5,43.1,-68.5Z"
+                transform="translate(100 100)"
+                fill="none" stroke="none"
+                pathLength="100"
+              />
+
+              <text className="text-content">
+                <textPath href="#text" startOffset="0%">FOUNDER. BUILDER. THINKER. FOUNDER. BUILDER. THINKER. FOUNDER. BUILDER. THINKER. FOUNDER. BUILDER. THINKER.
+                  <animate attributeName="startOffset" from="0%" to="100%" dur="20s" repeatCount="indefinite" />
+                </textPath>
+                <textPath href="#text" startOffset="100%">FOUNDER. BUILDER. THINKER. FOUNDER. BUILDER. THINKER. FOUNDER. BUILDER. THINKER. FOUNDER. BUILDER. THINKER.
+                  <animate attributeName="startOffset" from="-100%" to="0%" dur="20s" repeatCount="indefinite" />
+                </textPath>
+              </text>
+            </svg>
           </motion.div>
         </section>
 
